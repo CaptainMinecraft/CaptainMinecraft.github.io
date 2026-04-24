2226cb41dff657e8090d125bf53535aa73b7f106
captainminecraft <captainminecraftog@gmail.com>
2026-04-24 21:59:09 +0500

    New file system
    Seperated Javascript code and Html code.

---
+++ Theme.js
@@ -0,0 +1,8 @@
+function NightMode() {
+  document.body.style.background="black"
+  document.body.text="white"
+}
+function LightMode() {
+  document.body.style.background="white"
+  document.body.text="black"
+ }
