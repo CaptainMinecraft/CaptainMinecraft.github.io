c6a65e897d99ebf4507a3dcd42c7e962488ee8ae
captainminecraft <captainminecraftog@gmail.com>
2026-04-26 12:05:32 +0500

    Deleted Theme.js
    File changed

--- Theme.js
+++
@@ -1,8 +0,0 @@
-function NightMode() {
-  document.body.style.background="black"
-  document.body.text="white"
-}
-function LightMode() {
-  document.body.style.background="white"
-  document.body.text="black"
- }
